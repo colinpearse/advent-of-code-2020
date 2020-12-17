748eb8a9b00aec575a4bca381d98d604cf902c30
Colin Pearse <colin@thepearses.com>
2020-12-17 13:42:58 -0600

    update

--- aofc2020.q17.py
+++ aofc2020.q17.py
@@ -4,7 +4,13 @@
 
 # NOTE: the 3x3 examples given don't match the same x,y, they are offset, because they are only showing active cubes
 
-import os
+# REFACTOR: stopped refactoring for multiple dimensions because check_neighbours(check,actives,dim=4)
+#           ran too slowly. I tried using operator and numpy and it didn't improve the speed:
+#           neighbour = tuple(map(operator.add, check, coords))
+#           neighbour = tuple(numpy.add(check, coords))
+#           Will have to revisit...
+
+import os, itertools
 
 def init_actives(cube, dim=3):
     actives = set()
@@ -12,34 +18,30 @@ def init_actives(cube, dim=3):
     for y in range(ylen):
         for x in range(xlen):
             if cube[y][x] == '#':
-                if dim == 4:
-                    actives.add((x,ylen-(y+1),0,0))
-                else: # dim == 3
-                    actives.add((x,ylen-(y+1),0))  # reverse y to make the top of the square the higher number
+                actives.add((x,ylen-(y+1))+(0,)*(dim-2))  # reverse y to make the top of the square the higher number
     return actives
 
+# return: [[-2,-1,0,1], [0,1,2,3], ... ] corresponding to x,y,...etc needing to be checked
 def extend_limits(actives, dim=3):
-    xmin,xmax,ymin,ymax,zmin,zmax,wmin,wmax = 1,-1,1,-1,1,-1,1,-1
-    xs,ys,zs,ws = [],[],[],[]
+    minmax = [[1,-1]]*dim   # [[1, -1], [1, -1], [1, -1], [1, -1], ...]
     for active in actives:
-        if dim == 4: x,y,z,w = active
-        else:        x,y,z = active; w = 0
-        xmin,xmax = min(x,xmin),max(x,xmax)
-        ymin,ymax = min(y,ymin),max(y,ymax)
-        zmin,zmax = min(z,zmin),max(z,zmax)
-        wmin,wmax = min(w,wmin),max(w,wmax)
-    limits = [list(range(xmin-1,xmax+2)),list(range(ymin-1,ymax+2)),list(range(zmin-1,zmax+2)),list(range(wmin-1,wmax+2))]
-    return limits[:dim]
+        for i in range(len(active)):
+            vdim = active[i]   # vdim will have x,y,z,w,... etc values
+            cmin,cmax = minmax[i][0],minmax[i][1]
+            minmax[i][0],minmax[i][1] = min(vdim,cmin),max(vdim,cmax)
+    return [list(range(minmax[i][0]-1,minmax[i][1]+2)) for i in range(len(minmax))]
 
-def check_neighbours(x,y,z,actives):
+# check_neighbours(..., dim=4) is SLOW compared to check_neighbours_4d()
+def check_neighbours(check,actives,dim=3):
     acount = 0
-    for xp in [-1,0,1]:
-        for yp in [-1,0,1]:
-            for zp in [-1,0,1]:
-                if (xp,yp,zp) != (0,0,0) and (x+xp,y+yp,z+zp) in actives:
-                    acount += 1
+    for coords in itertools.product(range(-1, 2),repeat=dim):
+        if coords != (0,)*dim:
+            neighbour = tuple(map(sum, zip(check, coords)))
+            if neighbour in actives:
+                acount += 1
     return acount
-def check_neighbours_4d(x,y,z,w,actives):
+def check_neighbours_4d(check,actives,dim=4):
+    x,y,z,w = check
     acount = 0
     for xp in [-1,0,1]:
         for yp in [-1,0,1]:
@@ -57,7 +59,7 @@ def cycle_cube(cube, ncycles):
         for x in xs:
             for y in ys:
                 for z in zs:
-                    acount = check_neighbours(x,y,z,actives)
+                    acount = check_neighbours((x,y,z),actives,dim=3)
                     if (x,y,z) in actives:
                         if acount == 2 or acount == 3:
                             new_actives.add((x,y,z))
@@ -76,7 +78,7 @@ def cycle_cube_4d(cube, ncycles):
             for y in ys:
                 for z in zs:
                     for w in ws:
-                        acount = check_neighbours_4d(x,y,z,w,actives)
+                        acount = check_neighbours_4d((x,y,z,w),actives,dim=4)
                         if (x,y,z,w) in actives:
                             if acount == 2 or acount == 3:
                                 new_actives.add((x,y,z,w))
